102b631eb7653d0dbe65ab56e861b6ed9badf3ef
Luna <imlvnaa@gmail.com>
2024-04-29 12:07:36 -0400

    move to /

--- src/routes/api/public/user/[userId=snowflake]/members/+server.ts
+++
@@ -1,7 +0,0 @@
-import simplyPluralGet from '$lib/server/sp';
-import { json, type RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = async ({ locals }) => {
-	const { id } = await simplyPluralGet('me', locals.spToken);
-	return json(await simplyPluralGet(`members/${id}`, locals.spToken));
-};
